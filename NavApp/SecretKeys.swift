1bf550588cdbce55dd63e01862691caa237ec983
Jeffrey Almonte <jalmonte83@gmail.com>
2019-02-12 18:50:52 -0500

    commit to my branch.

--- NavApp/SecretKeys.swift
+++ NavApp/SecretKeys.swift
@@ -2,13 +2,13 @@
 //  SecretKeys.swift
 //  NavApp
 //
-//  Created by Ashli Rankin on 2/11/19.
+//  Created by Jeffrey Almonte on 2/12/19.
 //  Copyright © 2019 Ashli Rankin. All rights reserved.
 //
 
 import Foundation
 
-struct SecretKeys {
-  static let clientKey = "BWGVFBF4C0DFZLRMUWWAFWBVKVXQ2VNTMFZBF0IUHPZYBHA0"
-  static let clientId = "ZY5XTVT4MDRJE5MB2YQYRWCEBIPCVAUJEOGQU2HHJDV2TAI5"
+final class SecretKeys {
+    static let clientKey = "BWGVFBF4C0DFZLRMUWWAFWBVKVXQ2VNTMFZBF0IUHPZYBHA0"
+    static let clientId = "ZY5XTVT4MDRJE5MB2YQYRWCEBIPCVAUJEOGQU2HHJDV2TAI5"
 }
